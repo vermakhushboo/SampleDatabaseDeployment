499ce310201c111d67809e906ad8bc9def9dac35
Piyush Ajmani <piyushajmani5@gmail.com>
2018-10-26 03:09:21 +0530

    Changed db location
    Signed-off-by: Piyush Ajmani <piyushajmani5@gmail.com>

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize('shopdb', 'shopper', 'shoppass', {
-    host: 'localhost',
-    dialect: 'postgres',
-    pool: {
-        min: 0,
-        max: 5,
-    }
-})
+// const db = new Sequelize('shopdb', 'shopper', 'shoppass', {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     pool: {
+//         min: 0,
+//         max: 5,
+//     }
+// })
+
+const db = new Sequelize('postgres://kllzitsegqztqc:4616b79316270605f57307f300708dc29660bb2a8fb2e7e4cbe8b9d6ebffdf3f@ec2-54-225-115-234.compute-1.amazonaws.com:5432/d5vmre2fa70uh');
 
 const User = db.define('users', {
     id: {
